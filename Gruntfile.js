803c1ae4fb9fde8546144a352ca7eaadfd3e56a9
Ralf S. Engelschall <rse@engelschall.com>
2020-04-10 23:21:45 +0200

    use a decent global variable in browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
                     external: [
                     ],
                     browserifyOptions: {
-                        standalone: "Syntax",
+                        standalone: "hashTagSimilarity",
                         debug: false
                     }
                 }
